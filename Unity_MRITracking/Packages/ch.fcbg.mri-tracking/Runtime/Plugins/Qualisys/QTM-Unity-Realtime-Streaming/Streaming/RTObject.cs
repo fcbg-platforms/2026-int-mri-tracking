f9e9aa6b2c2895f3c40fae12b3ac046a8cbb2098
Mael Lacour <public.dev@maellacour.com>
2026-02-24 15:34:59 +0100

    Add Qualisys RealTime Streaming support with marker and skeleton handling
    - Implement RTMarkerStream for real-time marker visualization.
    - Create RTObject and RTObjectMarkers for handling 6DOF objects and their markers.
    - Introduce RTSkeleton for skeleton tracking and animation.
    - Add RTUnlabeledMarker for handling unlabeled markers.
    - Update package dependencies to include animation rigging tools.
    - Add assembly definitions for better project organization.
    - Include necessary metadata and configuration files for new scripts.

---
+++ Unity_MRITracking/Packages/ch.fcbg.mri-tracking/Runtime/Plugins/Qualisys/QTM-Unity-Realtime-Streaming/Streaming/RTObject.cs
@@ -0,0 +1,40 @@
+﻿// Unity SDK for Qualisys Track Manager. Copyright 2015-2018 Qualisys AB
+//
+using UnityEngine;
+
+namespace QualisysRealTime.Unity
+{
+    public class RTObject : MonoBehaviour
+    {
+        public string ObjectName = "Put QTM 6DOF object name here";
+        public Vector3 PositionOffset = new Vector3(0, 0, 0);
+        public Vector3 RotationOffset = new Vector3(0, 0, 0);
+
+        protected RTClient rtClient;
+
+        protected virtual void applyBodyTransform(SixDOFBody body)
+        {
+            if (!float.IsNaN(body.Position.sqrMagnitude)) //just to avoid error when position is NaN
+            {
+                transform.localRotation = body.Rotation * Quaternion.Euler(RotationOffset);
+                transform.localPosition = body.Position + PositionOffset;                
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            rtClient = RTClient.GetInstance();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rtClient == null) rtClient = RTClient.GetInstance();
+
+            var body = rtClient.GetBody(ObjectName);
+            if (body != null)
+                applyBodyTransform(body);
+        }
+    }
+}
